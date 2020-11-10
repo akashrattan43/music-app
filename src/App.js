4c3c3c5e779be291d30b41bffb51ca2a882f4575
akashrattan43 <akashrattan43@gmail.com>
2020-11-09 17:54:49 -0800

    icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import Player from './components/Player'
+import Song from './components/Song'
 
 function App() {
   return (
     <div className="App">
-      <h1>Music App</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
