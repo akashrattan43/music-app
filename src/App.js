756199abff027c103d0db08d48991aa369173f4e
akash rattan <akashrattan43@Gmail.com>
2020-11-09 20:30:19 -0800

    song can play and pause

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import Player from './components/Player'
-import Song from './components/Song'
-import './styles/app.scss'
-import data from './util'
+import userEvent from "@testing-library/user-event";
+import React, { useState } from "react";
+//adding components
+import Player from "./components/Player";
+import Song from "./components/Song";
+//adding styles
+import "./styles/app.scss";
+//adding data from util file where songs excist
+import data from "./util";
 
 function App() {
-  //state 
-  const [songs, setSongs] = useState(data())
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong}/>
+      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong}/>
     </div>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,25 @@
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  //useRef
+  const audioRef = useRef(null);
+  //event Handler
+  const playSongHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying)
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying)
+    }
+  };
+
   return (
     <div className="player">
       <div className="time-control">
@@ -10,10 +28,20 @@ const Player = () => {
         <p>End Time</p>
       </div>
       <div className="play-control">
-        <FontAwesomeIcon className='skip-forward' size='2x' icon={faAngleLeft}/>
-        <FontAwesomeIcon className='play' size='2x' icon={faPlay}/>
-        <FontAwesomeIcon className='skip-back' size='2x' icon={faAngleRight}/>
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleLeft}
+        />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleRight} />
       </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 };
