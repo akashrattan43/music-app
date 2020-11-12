587ed2460eb0853901499d764f7ccd21fa3f2770
akash rattan <akashrattan43@Gmail.com>
2020-11-11 20:43:58 -0800

    change songs

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,14 +1,22 @@
-import LibrarySong from './LibrarySong'
+import LibrarySong from "./LibrarySong";
 
-const Library = ({songs}) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
-          {songs.map(song => <LibrarySong song={song}/>)}
+        {songs.map((song) => (
+          <LibrarySong
+            songs={songs}
+            setCurrentSong={setCurrentSong}
+            song={song}
+            id={song.id}
+            key={song.id}
+          />
+        ))}
       </div>
     </div>
   );
 };
 
-export default Library
+export default Library;
