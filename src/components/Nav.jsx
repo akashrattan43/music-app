c5a83541ade78f7bb25657771ff0c158f39ae6e8
akash rattan <akashrattan43@Gmail.com>
2020-11-12 19:49:12 -0800

    finished

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { faMusic } from '@fortawesome/free-solid-svg-icons'
 const Nav = ({setLibraryStatus, libraryStatus}) => {
     return(
         <nav>
-            <h1>Waves</h1>
+            <h1>Chill Music</h1>
             <button onClick={() => setLibraryStatus(!libraryStatus)}>
                 Library
                 <FontAwesomeIcon icon={faMusic} />
